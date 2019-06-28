737047024b8147ae780c5c1fa7a75096a2af9d5a
Robson P Oliveira <rpopuc@gmail.com>
2019-06-28 07:05:57 -0300

    Adiciona sucrase e nodemon ao projeto
    - Para suporte á sintaxe import

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const routes = require('./routes')
+import express from 'express'
+import routes from './routes'
 
 class App {
 
@@ -27,4 +27,4 @@ class App {
 
 
 // Exporta a instância de server
-module.exports = new App().server;
+export default new App().server;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const app = require('./app');
+import app from './app';
 
 
 // Inicia o servidor na porta 3333
