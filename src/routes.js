0cc5e22303e7509376b1f0ed189a5dc18aef8287
Robson P Oliveira <rpopuc@gmail.com>
2019-06-29 20:34:55 -0300

    Adiciona agendamento
    - Cria controller AppointmentController;
    - Cria rota para inclusão de agendamento em /appointments

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 // Obtém o middleware de autenticação
 import authMiddleware from './app/middlewares/auth';
@@ -25,5 +26,6 @@ routes.put('/users', authMiddleware, UserController.update);
 const upload = multer(multerConfig);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
