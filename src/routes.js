2ce910a27afbd4b9e994ca1d175420e77304e393
Robson P Oliveira <rpopuc@gmail.com>
2019-06-29 13:05:17 -0300

    Criada configuração para acesso aos Models

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
+
+routes.get('/test', async (req, res) => {
+  const user = await User.create({
+    name: 'Teste',
+    email: 'teste@localhost.dev',
+    password_hash: 'teste',
+  });
+
+  return res.json(user);
+});
 
 // Define a rota principal da aplicação
 routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
