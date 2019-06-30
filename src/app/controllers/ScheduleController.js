ea7fdc47c04e4c4ccd661cb4fce6b91039816284
Robson P Oliveira <rpopuc@gmail.com>
2019-06-29 21:14:00 -0300

    Adiciona listagem de agendamentos para provedor

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,43 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+// Importa a biblioteca de validação
+// Importa o model do Usuário/
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+/**
+ * Controller para gestão de dados de usuários
+ */
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true,
+      },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
