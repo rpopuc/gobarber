92676987848ce4610286e7d84b7bf1a075832e0a
Robson P Oliveira <rpopuc@gmail.com>
2019-06-30 09:49:50 -0300

    Adiciona integração com Sentry
    - Adiciona controle de erros com Youch

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import sentryConfig from './config/sentry';
+import 'express-async-errors';
 import routes from './routes';
 
 import './database';
@@ -9,11 +13,18 @@ class App {
     // Instancia o expres
     this.server = express();
 
+    Sentry.init(sentryConfig);
+    this.server.use(Sentry.Handlers.requestHandler());
+
     // Carrega os middlewares
     this.middlewares();
 
     // Carrega as rotas
     this.routes();
+
+    this.server.use(Sentry.Handlers.errorHandler());
+
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -29,6 +40,13 @@ class App {
   routes() {
     // Configura as rotas para a aplicação
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
